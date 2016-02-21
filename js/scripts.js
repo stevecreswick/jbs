7bb037e7b85750a26ee83de8e8cfeee88118661d
Steve Creswick <screswick@gmail.com>
2016-02-21 12:47:33 -0500

    Add page layout

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,18 @@ console.log('loaded');
 
 $(function() {
 
+
    $("body").mousewheel(function(event, delta) {
-
       this.scrollLeft -= (delta * 30);
-
+      console.log(this.scrollLeft);
       event.preventDefault();
 
    });
+
+  // Horizontal Scroll
+  //  $(window).scroll(function(){
+  //    console.log(window.scrollX);
+  //  });
+
 
 });
