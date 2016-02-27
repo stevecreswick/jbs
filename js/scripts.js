80692fce6c63829c5e25f13de5d8b587792784cd
Steve Creswick <screswick@gmail.com>
2016-02-27 15:45:02 -0500

    add flex properties to main container

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,12 @@ console.log('loaded');
 $(function() {
 
 
-   $("body").mousewheel(function(event, delta) {
-      this.scrollLeft -= (delta * 30);
-      console.log(this.scrollLeft);
-      event.preventDefault();
-
-   });
+  //  $("body").mousewheel(function(event, delta) {
+  //     this.scrollLeft -= (delta * 30);
+  //     console.log(this.scrollLeft);
+  //     event.preventDefault();
+   //
+  //  });
 
   // Horizontal Scroll
   //  $(window).scroll(function(){
