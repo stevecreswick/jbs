2657413c7f707f435d79eef148fed642502f7565
Steve Creswick <screswick@gmail.com>
2016-03-08 20:48:53 -0500

    fix scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,62 @@ console.log('loaded');
 $(function() {
 
 
+  if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
+
+function wheel(event) {
+    var delta = 0;
+    if (event.wheelDelta) delta = event.wheelDelta / 120;
+    else if (event.detail) delta = -event.detail / 3;
+
+    handle(delta);
+    if (event.preventDefault) event.preventDefault();
+    event.returnValue = false;
+}
+
+function handle(delta) {
+    var time = 1000;
+	   var distance = 300;
+
+    $('html, body').stop().animate({
+        scrollLeft: $(window).scrollLeft() - (distance * delta)
+    }, time );
+  }
+
+
+  // Control Scroll Speeds
+  $.fn.moveIt = function(){
+  var $window = $(window);
+  var instances = [];
+
+  $(this).each(function(){
+    instances.push(new moveItItem($(this)));
+  });
+
+  window.onscroll = function(){
+    var scrollLeft = $window.scrollLeft();
+    instances.forEach(function(inst){
+      inst.update(scrollLeft);
+    });
+  }
+}
+
+var moveItItem = function(el){
+  this.el = $(el);
+  this.speed = parseInt(this.el.attr('data-scroll-speed'));
+  console.log(this.speed);
+};
+
+moveItItem.prototype.update = function(scrollLeft){
+  var pos = scrollLeft / this.speed;
+  this.el.css('transform', 'translateX(' + -pos + 'px)');
+};
+
+  // Grab divs with scroll attribute set
+
+  // $('[data-scroll-speed]').moveIt();
+
+  // Horizontal Scroll
   //  $("body").mousewheel(function(event, delta) {
   //     this.scrollLeft -= (delta * 30);
   //     console.log(this.scrollLeft);
@@ -15,6 +71,11 @@ $(function() {
   //  $(window).scroll(function(){
   //    console.log(window.scrollX);
   //  });
+  //
+  $("html").niceScroll({
+    scrollspeed: 10, // scrolling speed
+    mousescrollstep: 3, // scrolling speed with mouse wheel (pixel)
+  });
 
 
 });
